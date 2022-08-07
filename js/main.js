e00fa43af68eba75acbd034fb5765c7fe4aedf93
email <abe.solis2015@gmail.com>
2022-08-06 20:42:56 -0700

    media queries

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ var swiper2 = new Swiper(".bg-slider", {
         swiper: swiper,
     },
 });
+
+//Navigation On Scroll 
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//mobile nav menu
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+
+menuBtn.addEventListener("click", () =>{
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+});
